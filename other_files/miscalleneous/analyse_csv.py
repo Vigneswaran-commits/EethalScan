a7d1313ce20c13f77e9e5680b464fd587911b972
Vigneswaran Ramamoorthy <70110132+Vigneswaran-commits@users.noreply.github.com>
2021-06-27 01:25:56 +0530

    Removed few files from Other folder

--- other_files/miscalleneous/analyse_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-df=pd.read_csv("C:/Users/vajravel/Desktop/Eethalview/other_files/model/dat/card4.csv")
-
-print(df.head(5))
-
-plt.scatter(df['Element Type'],df['Number of Direct Stress'])
